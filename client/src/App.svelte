4b49eff429f95b3c043ea39ccc01bbd8d33e73ab
Filip Gynthersen <filipg@live.dk>
2024-05-02 19:20:04 +0200

    initial client setup

---
+++ client/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+   import { Router, Route, Link } from 'svelte-routing';
+
+   import Login from "./pages/Login.svelte";
+   import Home from "./pages/Home.svelte";
+
+</script>
+
+<main>
+  <Router>
+    <Route path="/" component={Login} />
+  </Router>
+</main>
